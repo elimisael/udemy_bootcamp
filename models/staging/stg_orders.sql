8e7681ac7e7ff2426676f571f5eeb74ae14b650b
Eli Misael Manjarrez <elimisael@msn.com>
2026-04-17 19:43:41 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+select 
+    -- from orders
+    o.OrderID,
+    o.OrderDate,
+    o.ShipDate,
+    o.ShipMode,
+    o.OrderSellingPrice - o.OrderCostPrice as OrderProfit,
+    o.OrderCostPrice,
+    o.OrderSellingPrice,
+    -- from customer
+    c.CustomerName,
+    c.Segment,
+    c.Country,
+    -- from product
+    p.Category,
+    p.ProductName,
+    p.SubCategory
+from {{ ref('raw_orders') }} as o
+    left join {{ ref('raw_customers') }} as c
+        on o.CustomerID = c.CustomerID
+    left join {{ ref('raw_products') }} as p
+        on o.ProductID = p.ProductID
+-- from raw.orders
