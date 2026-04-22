8ce3d2db5e155d89db3c3758fafb372fc826ab67
Eli Misael Manjarrez <elimisael@msn.com>
2026-04-22 17:03:11 +0000

    added delivery team and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
     -- from orders
+    {{ dbt_utils.generate_surrogate_key(['o.OrderID', 'c.CustomerID', 'p.ProductID']) }} as sk_orders,
     o.OrderID,
     o.OrderDate,
     o.ShipDate,
@@ -17,10 +18,14 @@ select
     p.Category,
     p.ProductName,
     p.SubCategory,
-    {{ markup() }} as MarkUp
+    {{ markup('o.OrderSellingPrice', 'o.OrderCostPrice') }} as MarkUp,
+    dt.delivery_team
 from {{ ref('raw_orders') }} as o
     left join {{ ref('raw_customers') }} as c
         on o.CustomerID = c.CustomerID
     left join {{ ref('raw_products') }} as p
         on o.ProductID = p.ProductID
+    left join {{ ref('delivery_team') }} as dt 
+       on o.ShipMode = dt.ShipMode
+{{ limit_data_in_dev('OrderDate')}}
 -- from raw.orders
